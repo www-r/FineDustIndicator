39c920e056a2bf4d874f52e3fd75c0ea930cdf80
youngeun <jessicak0828@gmail.com>
2023-09-25 00:49:46 +0900

    feat: Card 컴포넌트 구조 잡기

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import * as S from './styled'
+export default function Card() {
+  return (
+    <S.Card>Card</S.Card>
+  )
+}
---
+++ src/components/Card/styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Card = styled.div`
+	width: 80%;
+	min-width: 10rem;
+	height: 10rem;
+`;
