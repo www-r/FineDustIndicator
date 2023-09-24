4bd509af79260564c985b68175a0784a5efc56ed
youngeun <jessicak0828@gmail.com>
2023-09-25 01:00:49 +0900

    feat: Pages에 내지역,모든지역,즐겨찾기지역 페이지 추가

---
+++ src/pages/MyLocPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Display from '../components/Display';
+import Navbar from '../components/Navbar';
+export default function MyLocPage() {
+	return (
+		<div>
+			<Display />
+			<Navbar />
+		</div>
+	);
+}
