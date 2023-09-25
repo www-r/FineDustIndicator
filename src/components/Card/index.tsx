9e7732c313722b9549fabb04c9cf5eaad098e429
김영은 <jessicak0828@gmail.com>
2023-09-25 17:17:27 +0900

    feat: Card 컴포넌트 퍼블리싱 완료

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import * as S from './styled'
+import React from 'react';
+import * as S from './styled';
+
 export default function Card() {
-  return (
-    <S.Card>Card</S.Card>
-  )
+	return (
+		<S.Card>
+			<S.SideContainer>
+				<S.Address>
+					<S.AddressRoad>도산대로</S.AddressRoad>
+					<S.AddressCity>서울</S.AddressCity>
+				</S.Address>
+				<div>
+					<span></span>
+				</div>
+			</S.SideContainer>
+			<S.MainContainer>
+				<S.FineDustDensity>보통!!!</S.FineDustDensity>
+				<S.FineDustRate>
+					미세먼지 수치: <span>47</span>
+				</S.FineDustRate>
+				<S.FineDustMeasureTime>2022-08-12 17:00 기준</S.FineDustMeasureTime>
+			</S.MainContainer>
+		</S.Card>
+	);
 }
--- src/components/Card/styled.ts
+++ src/components/Card/styled.ts
@@ -1,7 +1,60 @@
 import styled from '@emotion/styled';
-
-export const Card = styled.div`
-	width: 80%;
+import { commons } from '../../style/commons';
+export const Card = styled.li`
+	box-shadow: 0 0 0 1px rgb(0 0 0 / 7%), 0 2px 4px rgb(0 0 0 / 5%), 0 12px 24px rgb(0 0 0 / 5%);
+	border: 1px solid transparent;
+	border-radius: 1rem;
+	margin: 1rem auto;
+	width: 20rem;
 	min-width: 10rem;
-	height: 10rem;
+	height: fit-content;
+	display: grid;
+	grid-template-rows: 1fr 3fr;
+	color: ${commons.color.white};
+	/* display: flex;
+	flex-direction: column; */
 `;
+export const SideContainer = styled.div`
+	border: 1px solid transparent;
+	border-top-right-radius: 1rem;
+	border-top-left-radius: 1rem;
+	display: flex;
+	justify-content: space-between;
+	background-color: ${commons.color.white};
+	color: ${commons.color.black};
+`;
+export const MainContainer = styled.div`
+	border: 1px solid transparent;
+	border-bottom-right-radius: 1rem;
+	border-bottom-left-radius: 1rem;
+	background-color: ${commons.color.pm10grade1};
+	text-align: center;
+	padding: 1rem 0;
+	/* display: flex;
+	flex-direction: column; */
+`;
+export const Address = styled.div`
+	margin: 0.8rem 0 0 0.8rem;
+`;
+export const AddressRoad = styled.span`
+	margin-right: 0.4rem;
+	font-size: 1rem;
+`;
+export const AddressCity = styled.span`
+	font-size: 1.3rem;
+`;
+export const FineDustDensity = styled.h2`
+	border: 1px solid transparent;
+	border-radius: 1rem;
+	font-size: 2.5rem;
+	font-weight: ${commons.fontWeight.bold};
+	background-color: ${commons.color.white};
+	margin: 0 auto;
+	padding: 0.5rem 1rem;
+	width: fit-content;
+	color: ${commons.color.pm10grade1};
+`;
+export const FineDustRate = styled.p`
+	margin-top: 0.8rem;
+`;
+export const FineDustMeasureTime = styled.p``;
