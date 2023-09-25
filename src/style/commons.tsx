dfb0208c5b38560156df4cb1f86c8f63840d4780
youngeun <jessicak0828@gmail.com>
2023-09-25 13:06:07 +0900

    feat: styles 에 Common 변수, 변수값 추가

---
+++ src/style/commons.tsx
@@ -0,0 +1,25 @@
+export const Commons = {
+	backgroundColor: {
+		pm10grade1: '#43B9E3', //best
+		pm10grade2: '#ACC11B', //normal
+		pm10grade3: '#FFB400', //not bad
+		pm10grade4: '#cf161f', //bad
+		pm10grade5: '#564b48', //worst
+	},
+	color: {
+		white: '#e5e5e5',
+		black: '#000',
+	},
+	fontSize: {
+		fs10: '1rem',
+		fs14: '1.4rem',
+		fs16: '1.6rem',
+		fs20: '2rem',
+	},
+	fontWeight: {
+		light: '400',
+		normal: '500',
+		semiBold: '650',
+		bold: '800',
+	},
+};
