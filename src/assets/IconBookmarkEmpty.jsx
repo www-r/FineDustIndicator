fd596834c96ba9e712565929cca440057fb0ccf1
김영은 <jessicak0828@gmail.com>
2023-09-25 17:16:30 +0900

    feat: 아이콘  svg 추가

---
+++ src/assets/IconBookmarkEmpty.jsx
@@ -0,0 +1,14 @@
+export default (
+	<svg width="80px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+		<g id="SVGRepo_iconCarrier">
+			<path
+				d="M17 3C18.1046 3 19 3.89543 19 5L19 19.0536C19 20.5893 17.341 21.552 16.0077 20.7901L12.9923 19.067C12.3774 18.7157 11.6226 18.7157 11.0077 19.067L7.99228 20.7901C6.65897 21.552 5 20.5893 5 19.0536L5 5C5 3.89543 5.89543 3 7 3L17 3Z"
+				stroke="#1C274C"
+				strokeWidth="2.4"
+				strokeLinejoin="round"
+			></path>
+		</g>
+	</svg>
+);
