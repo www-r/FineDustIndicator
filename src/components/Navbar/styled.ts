f051b101cb1c440e04f9bb2aa5a9455cb874a23b
김영은 <jessicak0828@gmail.com>
2023-10-02 17:31:42 +0900

    config: axios 설치

--- src/components/Navbar/styled.ts
+++ src/components/Navbar/styled.ts
@@ -2,12 +2,11 @@ import styled from '@emotion/styled';
 import { commons } from '../../style/commons';
 export const Navbar = styled.nav`
 	width: 100%;
-	height: 7rem;
+	height: fit-content;
 	background-color: ${commons.color.purple};
 	position: fixed;
 	bottom: 0;
 	right: 0;
-
 	ul {
 		height: 100%;
 		display: flex;
@@ -16,8 +15,8 @@ export const Navbar = styled.nav`
 	}
 `;
 export const NavbarItem = styled.li`
+	padding-top: 0.5rem;
 	width: calc(100% / 3);
-	height: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
