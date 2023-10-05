b8cb9f9fc45383799a274e4dc0610f447bf2832c
youngeun <jessicak0828@gmail.com>
2023-10-05 14:45:20 +0900

    feat: Card 입력값 수정

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import * as S from './styled';
 
-export default function Card() {
+interface CardProps {
+
+		sidoName: string;
+		stationName: string;
+		dataTime: string;
+		pm10Grade: string;
+		pm10Value: string;
+
+}
+
+export default function Card({ sidoName,stationName,dataTime,pm10Grade,pm10Value}: CardProps) {
+	const getPm10Grade = pm10Grade => {
+		switch (pm10Grade) {
+			case '1':
+				return '좋음';
+			case '2':
+				return '보통';
+			case '3':
+				return '한때나쁨';
+			case '4':
+				return '나쁨';
+			case '5':
+				return '매우나쁨';
+		}
+	};
 	return (
 		<S.Card>
 			<S.SideContainer>
 				<S.Address>
-					<S.AddressRoad>도산대로</S.AddressRoad>
-					<S.AddressCity>서울</S.AddressCity>
+					<S.AddressRoad>{stationName}</S.AddressRoad>
+					<S.AddressCity>{sidoName}</S.AddressCity>
 				</S.Address>
 				<div>
 					<span></span>
 				</div>
 			</S.SideContainer>
 			<S.MainContainer>
-				<S.FineDustDensity>보통!!!</S.FineDustDensity>
+				<S.FineDustDensity>{getPm10Grade(pm10Grade)}</S.FineDustDensity>
 				<S.FineDustRate>
-					미세먼지 수치: <span>47</span>
+					미세먼지 수치: <span>{pm10Value}</span>
 				</S.FineDustRate>
-				<S.FineDustMeasureTime>2022-08-12 17:00 기준</S.FineDustMeasureTime>
+				<S.FineDustMeasureTime>{dataTime} 기준</S.FineDustMeasureTime>
 			</S.MainContainer>
 		</S.Card>
 	);
