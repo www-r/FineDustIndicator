2de89e755d667e0929d61d5bcf408e1f95ecfffb
youngeun <jessicak0828@gmail.com>
2023-10-10 23:13:44 +0900

    feat: 강사님 코드와 내 코드 병합
    favoriteSlice, locationSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,29 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { locationSlice } from './slices/locationSlice';
+import {favLocationsSlice} from './slices/favoriteSlice'
 
-const initialMyLocationState = { sidoName: '', stationName: '' };
-const initialFavLocationsState = []
-const myLocationSlice = createSlice({
-	name: 'myLocationSlice',
-	initialState: initialMyLocationState,
-	reducers: {
-		update(state, action) {
-			state.myLocation = action.myLocation;
-		},
-		reset(state) {
-			state.myLocation = { sidoName: '', stationName: '' };
-		}
-	}
-});
-const favLocationsSlice = createSlice({
-	name: 'favLocationsSlice',
-	initialState: initialFavLocationsState,
-	reducers: {
-		add(state, action) {
-			state.favLocations = [
-				...state.favLocations,
-				action.favLocation
-			];
-		},
-		remove(state, action) {
-			state.favLocations = state.favLocations.filter(
-				item => item !== action.favLocation
-			);
-		},
-		reset(state){
-			state.favLocations = []
-		}
-	}
-});
+// const initialMyLocationState = { sidoName: '', stationName: '' };
+// const initialFavLocationsState = [];
+// const myLocationSlice = createSlice({
+// 	name: 'myLocationSlice',
+// 	initialState: initialMyLocationState,
+// 	reducers: {
+// 		update(state, action) {
+// 			state.myLocation = action.payload;
+// 		},
+// 		reset(state) {
+// 			state.myLocation = { sidoName: '', stationName: '' };
+// 		}
+// 	}
+// });
+
 const store = configureStore({
 	reducer: {
-		myLocationSlice,
-		favLocationsSlice,	}
+		// myLocationSlice,
+		locationSlice,
+		favLocationsSlice
+	}
 });
-export const myLocationActions = myLocationSlice.actions;
-export const favLocationsActions = favLocationsSlice.actions;
+// export const myLocationActions = myLocationSlice.actions;
+
 export default store;
