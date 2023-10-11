a33d0bf39d20db604aeefa729abaec7430f688c8
youngeun <jessicak0828@gmail.com>
2023-10-11 15:46:27 +0900

    feat: reducer 와 api 요청을 통해 나의 위치 가져오기

--- src/pages/MyLocPage.tsx
+++ src/pages/MyLocPage.tsx
@@ -1,25 +1,50 @@
-import React , {useState} from 'react';
-import Display from '@/components/Display';
-import Searchbar from '@/components/Searchbar';
-import Card from '@/components/Card';
-import Navbar from '@/components/Navbar';
+import React, { useState, useEffect } from 'react';
+import Display from '../components/Display';
+import Searchbar from '../components/Searchbar';
+import Card from '../components/Card';
+import Navbar from '../components/Navbar';
 import * as S from './styled';
+import { useLocationSlice, setMyLocation } from '../store/slices/locationSlice';
 import { getData } from '../utils/api';
-import { getDate } from '../utils/getDate';
-export default function MyLocPage() {
 
-	// navigator.geolocation.getCurrentPosition((data: GeolocationPosition) => {
-	// 	const { latitude, longitude } = data.coords;
-	// 	console.log(latitude, longitude);
-	// });
-	getData(getDate(), '서울');
+export default function MyLocPage() {
+	const { myLocation, dispatch } = useLocationSlice();
+	const [myLocationData, setMyLocationData] = useState({
+		sidoName: myLocation.sidoName,
+		stationName: myLocation.stationName,
+		dataTime: '',
+		pm10Grade: '',
+		pm10Value: ''
+	});
+
+	const getMyLocationData = async myLocation => {
+		const res = await getData(myLocation.sidoName); //배열
+		const myLocData = res.find(
+			({ stationName }) => stationName === myLocation.stationName
+		);
+		console.log('getMyLocationData:', myLocData);
+		setMyLocationData(myLocData);
+		dispatch(setMyLocation(myLocData));
+	};
+
+	useEffect(() => {
+		getMyLocationData(myLocation);
+	}, []);
 
 	return (
 		<S.Page>
 			<Searchbar />
 			<Display>
 				<ul>
-					<Card />
+					{
+						<Card
+							sidoName={myLocationData?.sidoName}
+							stationName={myLocationData?.stationName}
+							dataTime={myLocationData?.dataTime}
+							pm10Grade={myLocationData?.pm10Grade}
+							pm10Value={myLocationData?.pm10Value}
+						/>
+					}
 				</ul>
 			</Display>
 			<Navbar />
