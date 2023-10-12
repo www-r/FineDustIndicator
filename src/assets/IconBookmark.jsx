66bee52ecddaad59501aefb5473f9d71d9dbf44c
youngeun <jessicak0828@gmail.com>
2023-10-12 23:47:35 +0900

    chore: 아이콘svg 크기 조정

--- src/assets/IconBookmark.jsx
+++ src/assets/IconBookmark.jsx
@@ -1,13 +1,23 @@
 export default (
-	<svg width="60px" height="60px" viewBox="0 0 24 24" fill="#1C274C" xmlns="http://www.w3.org/2000/svg">
-		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-		<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-		<g id="SVGRepo_iconCarrier">
+	<svg
+		width='30px'
+		height='30px'
+		viewBox='0 0 24 24'
+		fill='#1C274C'
+		xmlns='http://www.w3.org/2000/svg'
+	>
+		<g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+		<g
+			id='SVGRepo_tracerCarrier'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		></g>
+		<g id='SVGRepo_iconCarrier'>
 			<path
-				d="M17 3C18.1046 3 19 3.89543 19 5L19 19.0536C19 20.5893 17.341 21.552 16.0077 20.7901L12.9923 19.067C12.3774 18.7157 11.6226 18.7157 11.0077 19.067L7.99228 20.7901C6.65897 21.552 5 20.5893 5 19.0536L5 5C5 3.89543 5.89543 3 7 3L17 3Z"
-				stroke="#1C274C"
-				strokeWidth="2.4"
-				strokeLinejoin="round"
+				d='M17 3C18.1046 3 19 3.89543 19 5L19 19.0536C19 20.5893 17.341 21.552 16.0077 20.7901L12.9923 19.067C12.3774 18.7157 11.6226 18.7157 11.0077 19.067L7.99228 20.7901C6.65897 21.552 5 20.5893 5 19.0536L5 5C5 3.89543 5.89543 3 7 3L17 3Z'
+				stroke='#1C274C'
+				strokeWidth='2.4'
+				strokeLinejoin='round'
 			></path>
 		</g>
 	</svg>
