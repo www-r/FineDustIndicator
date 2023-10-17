5a5a4c07f66920e7a9580f4edb14f46d5f19f0eb
김영은 <jessicak0828@gmail.com>
2023-10-17 19:26:06 +0900

    feat: 상수 sidoArr 추가

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import ListBox from '../ListBox';
 import * as S from './styled';
+
 export default function Searchbar() {
-	const sidosArr = [
-		'서울',
-		'부산',
-		'대구',
-		'인천',
-		'광주',
-		'대전',
-		'울산',
-		'경기',
-		'강원',
-		'충북',
-		'충남',
-		'전북',
-		'전남',
-		'경북',
-		'경남',
-		'제주',
-		'세종',
-	];
 	return (
 		<S.Searchbar>
-			<ListBox array={sidosArr} />
-			<ListBox array={[]} />
+			<ListBox />
 		</S.Searchbar>
 	);
 }
