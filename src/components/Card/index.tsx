ba4e436a02868570831228c8785a9d63051e86ec
김영은 <jessicak0828@gmail.com>
2023-10-18 14:45:03 +0900

    feat: bookmark btn 생성

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled';
+import IconBookmarkEmpty from '../../assets/IconBookmarkEmpty';
+import IconBookmark from '../../assets/IconBookmark';
 import { LocationData } from '../../interface';
 
-
-export default function Card({
-	sidoName,
-	stationName,
-	dataTime,
-	pm10Grade,
-	pm10Value
-}: LocationData) {
-	const getPm10GradeString = pm10Grade => {
+export default function Card({ sidoName, stationName, dataTime, pm10Grade, pm10Value }: LocationData) {
+	const [isBookmarked, setIsBookmarked] = useState(false);
+	const getPm10GradeString = (pm10Grade) => {
 		switch (pm10Grade) {
 			case '1':
 				return '좋음';
@@ -24,7 +20,15 @@ export default function Card({
 				return '매우나쁨';
 		}
 	};
+	const clickBookmarkBtn = () => {
+		if(isBookmarked){
+			
+		}
+		if(!isBookmarked){
 
+		}
+		setIsBookmarked(!isBookmarked);
+	};
 	return (
 		<S.Card>
 			<S.SideContainer>
@@ -32,11 +36,10 @@ export default function Card({
 					<S.AddressCity>{sidoName}</S.AddressCity>
 					<S.AddressRoad>{stationName}</S.AddressRoad>
 				</S.Address>
+				<button onClick={clickBookmarkBtn}>{isBookmarked ? IconBookmark : IconBookmarkEmpty}</button>
 			</S.SideContainer>
 			<S.MainContainer className={`grade${pm10Grade}`}>
-				<S.FineDustDensity className={`grade${pm10Grade}`}>
-					{getPm10GradeString(pm10Grade)}
-				</S.FineDustDensity>
+				<S.FineDustDensity className={`grade${pm10Grade}`}>{getPm10GradeString(pm10Grade)}</S.FineDustDensity>
 				<S.FineDustRate>
 					미세먼지 수치: <span>{pm10Value}</span>
 				</S.FineDustRate>
