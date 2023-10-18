a98b2f4b90833fb34222d45059b3da81b0a2d336
김영은 <jessicak0828@gmail.com>
2023-10-18 14:28:20 +0900

    feat: locationSlice TypeScript 작성

--- src/store/slices/locationSlice.ts
+++ src/store/slices/locationSlice.ts
@@ -1,26 +1,27 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
-import { MyLocation, AllLocation } from '../../interface';
+import { Location } from '../../interface';
 
 interface InitialState {
-	myLocation: MyLocation;
-	allLocation: AllLocation;
+	myLocation: Location;
+	allLocation: Location;
 }
 const initialState: InitialState = {
 	myLocation: { sidoName: '서울', stationName: '강남구' },
-	allLocation: { sidoName: '서울' },
+	allLocation: { sidoName: '서울', stationName: '' },
 };
 
 export const locationSlice = createSlice({
 	name: 'location',
 	initialState: initialState,
 	reducers: {
-		setMyLocation: (state, action: PayloadAction<MyLocation>) => {
+		setMyLocation: (state, action: PayloadAction<Location>) => {
 			state.myLocation.sidoName = action.payload.sidoName;
 			state.myLocation.stationName = action.payload.stationName;
 		},
-		setAllLocation: (state, action: PayloadAction<AllLocation>) => {
+		setAllLocation: (state, action: PayloadAction<Location>) => {
 			state.allLocation.sidoName = action.payload.sidoName;
+			state.allLocation.stationName = action.payload.stationName;
 		},
 	},
 });
@@ -28,8 +29,8 @@ export const locationSlice = createSlice({
 export const { setMyLocation, setAllLocation } = locationSlice.actions;
 
 export function useLocationSlice() {
-	const allLocation: AllLocation = useSelector((state) => state.location.allLocation);
-	const myLocation: MyLocation = useSelector((state) => state.location.myLocation);
+	const allLocation: Location = useSelector((state) => state.location.allLocation);
+	const myLocation: Location = useSelector((state) => state.location.myLocation);
 	const dispatch = useDispatch();
 
 	return {
