153094ff5ae2ce6152240ad4c3ce1ef7b6ff3a2a
김영은 <jessicak0828@gmail.com>
2023-10-18 14:30:54 +0900

    feat: ListBox를 Page type 에 따라 보이게/안보이게 하기

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ListBox from '../ListBox';
 import * as S from './styled';
+import { sidosArr } from '../../constants/sidosArr';
+import { useLocationSlice } from '../../store/slices/locationSlice';
+import { useStationSlice, setStationArr } from '../../store/slices/stationSlice';
+import { getSidoData } from '../../utils/api';
+export default function Searchbar({ show }) {
+	const [arr, setArr] = useState([]);
+	const { allLocation, dispatch } = useLocationSlice();
+	const { stationArr } = useStationSlice();
+	const getStations = async () => {
+		const res = await getSidoData(allLocation.sidoName);
+		const stationArr = res.map((item) => item.stationName);
+		setArr(stationArr);
+		dispatch(setStationArr(stationArr));
+		console.log(stationArr);
+	};
+	useEffect(() => {
+		getStations();
+	}, [allLocation.sidoName]);
 
-export default function Searchbar() {
 	return (
 		<S.Searchbar>
-			<ListBox />
-			<ListBox />
+			{show ? (
+				<>
+					<ListBox array={sidosArr} type="sido" />
+					<ListBox array={arr} type="station" />
+				</>
+			) : (
+				<></>
+			)}
 		</S.Searchbar>
 	);
 }
