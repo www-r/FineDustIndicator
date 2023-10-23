e12497f7d3094e762b10db47bd2a80b9a37cb580
youngeun <jessicak0828@gmail.com>
2023-10-24 02:57:27 +0900

    feat: AllLocsPage 에 안 쓰는 함수 제거

--- src/pages/AllLocsPage.tsx
+++ src/pages/AllLocsPage.tsx
@@ -5,44 +5,39 @@ import Card from '../components/Card';
 import Navbar from '../components/Navbar';
 import * as S from './styled';
 import { getSidoData } from '../utils/api';
-import { useLocationSlice, setAllLocation } from '../store/slices/locationSlice';
-import { LocationData } from '../interface';
+import {
+	useLocationSlice,
+} from '../store/slices/locationSlice';
+import { LocationData } from '../type';
 
 export default function AllLocsPage() {
 	const [dataArr, setDataArr] = useState<LocationData[]>([]);
 	const { allLocation } = useLocationSlice();
+
 	const getAllLocationDataArr = async () => {
 		const res = await getSidoData(allLocation.sidoName);
 		setDataArr(res);
 	};
-	const filterStationArr = () => {
-		const filteredArr = dataArr.filter((item) => item.stationName === allLocation.stationName);
-		setDataArr(filteredArr);
-	};
+
 	useEffect(() => {
 		getAllLocationDataArr();
 	}, [allLocation.sidoName]);
-	useEffect(() => {
-		filterStationArr();
-	}, [allLocation.stationName]);
+
 	return (
 		<S.Page>
 			<Searchbar show />
 			<Display>
-				{dataArr.map((locationData) =>
-					locationData.pm10Grade && locationData.pm10Value ? (
-						<Card
-							key={locationData?.stationName}
-							sidoName={locationData?.sidoName}
-							stationName={locationData?.stationName}
-							dataTime={locationData?.dataTime}
-							pm10Grade={locationData?.pm10Grade}
-							pm10Value={locationData?.pm10Value}
-						/>
-					) : (
-						<></>
-					)
-				)}
+				{dataArr.map(locationData => (
+					<Card
+						key={locationData.stationName}
+						sidoName={locationData.sidoName}
+						stationName={locationData.stationName}
+						dataTime={locationData.dataTime}
+						pm10Grade={locationData.pm10Grade}
+						pm10Value={locationData.pm10Value}
+						isPinned={locationData.isPinned}
+					/>
+				))}
 			</Display>
 			<Navbar />
 		</S.Page>
