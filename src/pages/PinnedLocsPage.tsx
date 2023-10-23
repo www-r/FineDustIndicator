c958ee4f50581735927d72c2bcbc0d6b1bf3aade
youngeun <jessicak0828@gmail.com>
2023-10-24 02:59:00 +0900

    feat: PinnedPage 생성
    컴포넌트 임포트

--- src/pages/PinnedLocsPage.tsx
+++ src/pages/PinnedLocsPage.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Searchbar from '../components/Searchbar';
+import Display from '../components/Display';
+import Card from '../components/Card';
+import Navbar from '../components/Navbar';
+import * as S from './styled';
+import { resetPin, usePinnedSlice } from '../store/slices/pinnedSlice';
+
 
 export default function PinnedLocsPage() {
-  return (
-    <div>PinnedLocsPage</div>
-  )
+	const { pinnedLocations} = usePinnedSlice();
+
+	return (
+		<S.Page>
+			<Searchbar show={false} />
+			<Display>
+				{pinnedLocations.map(element => (
+					<Card
+						sidoName={element.sidoName}
+						stationName={element.stationName}
+						dataTime={element.dataTime}
+						pm10Grade={element.pm10Grade}
+						pm10Value={element.pm10Value}
+						isPinned={element.isPinned}
+					/>
+				))}
+			</Display>
+			<Navbar />
+		</S.Page>
+	);
 }
