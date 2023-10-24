9fee57fc49d117484822099ac47897fd24ad1f4b
youngeun <jessicak0828@gmail.com>
2023-10-24 13:18:19 +0900

    feat: ListBox 에 상태관리 추가

--- src/components/ListBox/index.tsx
+++ src/components/ListBox/index.tsx
@@ -1,35 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './styled';
-import { useLocationSlice, setMyLocation, setAllLocation } from '../../store/slices/locationSlice';
+import {
+	useSearchbarSlice,
+	setSidoName,
+	setStationName,
+	setStationArr
+} from '../../store/slices/searchbarSlice';
+import { getSidoData } from '../../utils/api';
+import { sidosArr } from '../../constants/sidosArr';
+
 interface ListBoxProps {
-	array: string[];
 	type: 'sido' | 'station';
 }
-export default function ListBox({ array, type }: ListBoxProps) {
+export default function ListBox({ type }: ListBoxProps) {
+	const { sidoName, stationName, dispatch } = useSearchbarSlice();
+	const [btnValue, setBtnValue] = useState<string>('');
 	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const [value, setValue] = useState('');
-	const { allLocation, dispatch } = useLocationSlice();
+	const [array, setArray] = useState<string[]>([]);
+
+	const getStationArr = sidoName => {
+		const res = await getSidoData(sidoName);
+		const stationArr = res.map(item => item.stationName);
+		setArray(stationArr);
+	};
 	const clickListBtn = () => {
 		setIsOpen(!isOpen);
-	};
-	const clickListItem = (value) => {
-		setValue(value);
-		setIsOpen(!isOpen);
-		if (type === 'sido') {
-			dispatch(setAllLocation({ sidoName: value, stationName: allLocation.stationName }));
-		} else if (type === 'station') {
-			dispatch(setAllLocation({ sidoName: allLocation.sidoName, stationName: value }));
+		switch (type) {
+			case 'sido':
+				setArray(sidosArr);
+				
+				break;
+			case 'station':
+				getStationArr(sidoName);
+				break;
 		}
 	};
+	const clickListItem = async value => {
+		setIsOpen(!isOpen);
+		if (type === 'sido') {
+			dispatch(setSidoName(value));
+			dispatch(setStationName(''));
+			
+		}
+		if (type === 'station') {
+			if (!value) {
+				const res = await getSidoData(sidoName);
+				const stationArr = res.map(item => item.stationName);
+				dispatch(setStationArr(stationArr));
+			} else {
+				dispatch(setStationName(value));
+			}
+		}
+	};
+	useEffect(() => {
+		switch (type) {
+			case 'sido':
+				setBtnValue(sidoName);
+				break;
+			case 'station':
+				setBtnValue(stationName);
+				break;
+		}
+	}, []);
 	return (
 		<S.ListBoxWrapper>
 			<S.ListBoxBtn onClick={clickListBtn}>
-				<S.ListBoxValue>{value}</S.ListBoxValue>
+				<S.ListBoxValue>{btnValue}</S.ListBoxValue>
 				<S.ListBoxBtnArrow>{isOpen ? '▲' : '▼'}</S.ListBoxBtnArrow>
 			</S.ListBoxBtn>
 			{isOpen && (
 				<S.ListBox>
-					{array.map((item) => (
+					{array.map(item => (
 						<S.ListItem onClick={() => clickListItem(item)}>{item}</S.ListItem>
 					))}
 				</S.ListBox>
