d85a2a01c4fadb92c521b412bcf2aceea4a9e8da
youngeun <jessicak0828@gmail.com>
2023-10-24 13:19:00 +0900

    feat: Searchbar 에 상태관리 추가

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,31 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import ListBox from '../ListBox';
 import * as S from './styled';
-import { sidosArr } from '../../constants/sidosArr';
-import { useLocationSlice } from '../../store/slices/locationSlice';
-import { useStationSlice, setStationArr } from '../../store/slices/stationSlice';
+import { useSearchbarSlice } from '../../store/slices/searchbarSlice';
+import { setStationArr } from '../../store/slices/searchbarSlice';
 import { getSidoData } from '../../utils/api';
+
 export default function Searchbar({ show }) {
-	const [arr, setArr] = useState([]);
-	const { allLocation, dispatch } = useLocationSlice();
-	const { stationArr } = useStationSlice();
-	const getStations = async () => {
-		const res = await getSidoData(allLocation.sidoName);
-		const stationArr = res.map((item) => item.stationName);
-		setArr(stationArr);
+	const { sidoName, sidoArr, stationName, stationArr, dispatch } =
+		useSearchbarSlice();
+	const getStationArr = async (sidoName: string) => {
+		const res = await getSidoData(sidoName);
+		const stationArr = res.map(item => item.stationName);
 		dispatch(setStationArr(stationArr));
-		console.log(stationArr);
 	};
 	useEffect(() => {
-		getStations();
-	}, [allLocation.sidoName]);
+		getStationArr(sidoName);
+	}, [sidoName]);
 
 	return (
 		<S.Searchbar>
 			{show ? (
 				<>
-					<ListBox array={sidosArr} type="sido" />
-					<ListBox array={arr} type="station" />
+					<ListBox type='sido' />
+					<ListBox type='station' />
 				</>
 			) : (
 				<></>
